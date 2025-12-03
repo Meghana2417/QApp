8ac397b9e61640bdab12b57c277aa357af262c50
Meghana2417 <118287463+Meghana2417@users.noreply.github.com>
2025-12-03 07:28:36 +0530

    Update settings.py

--- QApp/settings.py
+++ QApp/settings.py
@@ -83,16 +83,26 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+  #  'default': {
+  #      'ENGINE': 'django.db.backends.postgresql',
+    #    'NAME': 'qappdb',
+    #    'USER': 'qappuser',
+   #     'PASSWORD': 'yourpassword',
+       # 'HOST': 'localhost',
+      #  'PORT': '5432',
+    #}
+#}
+import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'qappdb',
-        'USER': 'qappuser',
-        'PASSWORD': 'yourpassword',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASS"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
-}
 
 
 
