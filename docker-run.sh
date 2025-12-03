9650f86ed9303846c7ca58c65ca0a73b395721af
Meghana2417 <118287463+Meghana2417@users.noreply.github.com>
2025-12-03 07:20:17 +0530

    Create docker-run.sh

---
+++ docker-run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker stop qapp || true
+docker rm qapp || true
+
+docker build -t qapp:latest .
+
+docker run -d \
+  --name qapp \
+  -p 8000:8000 \
+  --env-file .env \
+  qapp:latest
+chmod +x docker-run.sh
