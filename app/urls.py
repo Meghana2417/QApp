e4055fe9c891a81d5d067db5aa84b257052357ef
Meghana2417 <118287463+Meghana2417@users.noreply.github.com>
2025-12-04 15:07:47 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,23 @@
-from django.http import JsonResponse
-from rest_framework.routers import DefaultRouter
+#from django.http import JsonResponse
+#from rest_framework.routers import DefaultRouter
+#from django.urls import path, include
+#from .views import *
+
+#router = DefaultRouter()
+#router.register(r'tags', TagViewSet, basename='tags')
+#router.register(r'posts', PostViewSet, basename='posts')
+#router.register(r'replies', ReplyViewSet, basename='replies')
+
+#def api_root(request):
+ #   return JsonResponse({"message": "Welcome to QApp API Root"})
+
+#urlpatterns = [
+  #   path('api/', api_root),
+   # path('api/', include(router.urls)),
+#]
+from django.http import HttpResponse
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import *
 
 router = DefaultRouter()
@@ -11,7 +28,11 @@ router.register(r'replies', ReplyViewSet, basename='replies')
 def api_root(request):
     return JsonResponse({"message": "Welcome to QApp API Root"})
 
+def home(request):
+    return HttpResponse("QApp is running successfully!")
+
 urlpatterns = [
-     path('api/', api_root),
+    path('', home),  # <-- ADD THIS
+    path('api/', api_root),
     path('api/', include(router.urls)),
 ]
